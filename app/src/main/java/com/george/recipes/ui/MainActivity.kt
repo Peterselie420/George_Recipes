88cc36e2cae612be06a2e2cfde61b180a6a7f989
PeterSelie <martijntjuuu@gmail.com>
2022-04-01 11:14:06 +0200

    Hilt, Timber and Leak Canary setup

--- app/src/main/java/com/george/recipes/ui/MainActivity.kt
+++ app/src/main/java/com/george/recipes/ui/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.george.recipes
+package com.george.recipes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.george.recipes.R
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val huts: String = "Huts"
+        Timber.d(huts)
     }
 }
